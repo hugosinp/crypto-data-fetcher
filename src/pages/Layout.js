d2316b85be4393817b69f25a7ef8279d06b0ea5e
hugosinp <hugo.anaponn@gmail.com>
2022-03-09 13:56:03 +0100

    refacto

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../components/molecules/Header'
+import Footer from '../components/molecules/Footer'
 
 const Layout = () => {
     return (
